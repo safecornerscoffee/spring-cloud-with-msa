683a82afb8a404734024d461c843442ab1700672
safecorners <safeconers.mobile@gmail.com>
2022-05-23 17:57:13 +0900

    게이트웨이 설정 방법을 DSL로 변경

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 ## Spring Cloud Gateway
 - [URL Rewriting With Spring Cloud Gateway](https://www.springcloud.io/post/2022-03/spring-cloud-gateway-url-rewriting/)
+> The RewritePath filter takes two arguments: a regular expression and a replacement string. The filter’s implementation works by simply executing the replaceAll() method on the request’s URI, using the provided parameters as arguments.
+
+> A caveat of the way that Spring handles configuration files is we can’t use the standard ${group} replacement expression, as Spring will think it is a property reference and try to replace its value. To avoid this, we need to add a backslash between the “$” and “{” characters that will be removed by the filter implementation before using it as the actual replacement expression.
+
+> A few remarks about the steps this code went through: Firstly, it calls the addOriginalRequestUrl(), which comes from the ServerWebExchangeUtils class, to store the original URL under the exchange’s attribute GATEWAY_ORIGINAL_REQUEST_URL_ATTR . The value of this attribute is a list to which we’ll append the received URL before going any modification and used internally by the gateway as part of the X-Forwarded-For header’s handling.
 - [Writing Custom Spring Cloud Gateway Filters](https://www.baeldung.com/spring-cloud-custom-gateway-filters)
 - [RewritePath Filter Factory](https://docs.spring.io/spring-cloud-gateway/docs/current/reference/html/#the-rewritepath-gatewayfilter-factory)
---
+++ services/gateway-server/src/main/java/com/safecornerscoffee/msa/gateway/FilterConfig.java
@@ -0,0 +1,73 @@
+package com.safecornerscoffee.msa.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.addOriginalRequestUrl;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r ->
+                        r.path("/users/**")
+                        .filters(f -> f.filter((exchange, chain) -> {
+                                ServerHttpRequest req = exchange.getRequest();
+                                addOriginalRequestUrl(exchange, req.getURI());
+                                String path = req.getURI().getRawPath();
+                                String newPath = path.replaceAll(
+                                        "/users/(?<segment>.*)",
+                                        "/${segment}"
+                                );
+                                ServerHttpRequest request = req.mutate().path(newPath).build();
+                                exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, request.getURI());
+                                return chain.filter(exchange.mutate().request(request).build());
+                            })
+                            .addRequestHeader("user-service-request", "user-service-request-header")
+                            .addResponseHeader("user-service-response", "user-service-response-header"))
+                        .uri("lb://user-service"))
+                .route(r ->
+                        r.path("/orders/**")
+                        .filters(f -> f.filter( (exchange, chain) -> {
+                                ServerHttpRequest req = exchange.getRequest();
+                                addOriginalRequestUrl(exchange, req.getURI());
+                                String path = req.getURI().getRawPath();
+                                String newPath = path.replaceAll(
+                                        "/orders/(?<sequence>.*)",
+                                        "/${sequence}"
+                                );
+                                ServerHttpRequest request = req.mutate().path(newPath).build();
+                                exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, request.getURI());
+                                return chain.filter(exchange.mutate().request(request).build());
+
+                            })
+                            .addResponseHeader("order-service-request", "order-service-request-header")
+                            .addResponseHeader("order-service-response", "order-service-response-header"))
+                        .uri("lb://order-service"))
+                .route(r ->
+                        r.path("/products/**")
+                                .filters(f -> f.filter( (exchange, chain) -> {
+                                    ServerHttpRequest req = exchange.getRequest();
+                                    addOriginalRequestUrl(exchange, req.getURI());
+                                    String path = req.getURI().getRawPath();
+                                    String newPath = path.replaceAll(
+                                            "/products/(?<sequence>.*)",
+                                            "/${sequence}"
+                                    );
+                                    ServerHttpRequest request = req.mutate().path(newPath).build();
+                                    exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, request.getURI());
+                                    return chain.filter(exchange.mutate().request(request).build());
+
+                                })
+                                .addResponseHeader("product-service-request", "product-service-request-header")
+                                .addResponseHeader("product-service-response", "product-service-response-header"))
+                        .uri("lb://product-service"))
+                .build();
+
+    }
+}
