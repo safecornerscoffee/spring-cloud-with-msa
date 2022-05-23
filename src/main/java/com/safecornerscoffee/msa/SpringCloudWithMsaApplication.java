cc626a30e6cb7d16c47b6c2e3b2684d2af370af3
safecorners <safeconers.mobile@gmail.com>
2022-05-23 13:36:25 +0900

    프로젝트 초기화

---
+++ src/main/java/com/safecornerscoffee/msa/SpringCloudWithMsaApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWithMsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudWithMsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecornerscoffee/msa/SpringCloudWithMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudWithMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
