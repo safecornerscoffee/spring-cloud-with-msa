0643aed9e615ee1c37c468f6ce848cb2d5410cd7
safecorners <safeconers.mobile@gmail.com>
2022-05-23 14:39:44 +0900

    유저 서비스 추가

---
+++ services/user-service/src/test/java/com/safecornerscoffee/msa/user/UserServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.msa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
