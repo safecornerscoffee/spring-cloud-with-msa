4acff9e4eb1f18d4e3b376742689378b2f9ae57b
safecorners <safeconers.mobile@gmail.com>
2022-05-23 15:06:32 +0900

    상품목록 서비스 추가

---
+++ services/product-service/src/test/java/com/safecornerscoffee/msa/product/ProductServiceApplicationTest.java
@@ -0,0 +1,15 @@
+package com.safecornerscoffee.msa.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class ProductServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
