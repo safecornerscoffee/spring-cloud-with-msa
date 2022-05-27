52f0b3d598e164b761f617511310b77cc3e28d66
safecorners <safeconers.mobile@gmail.com>
2022-05-27 21:26:31 +0900

    enable httptrace on gateway-server

--- services/gateway-server/src/main/java/com/safecornerscoffee/msa/gateway/GatewayServerApplication.java
+++ services/gateway-server/src/main/java/com/safecornerscoffee/msa/gateway/GatewayServerApplication.java
@@ -1,13 +1,21 @@
 package com.safecornerscoffee.msa.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public HttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
     }
 }
