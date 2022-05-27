5f569653e7b8335cd59e41145d5e8c0edbc03b8f
safecorners <safeconers.mobile@gmail.com>
2022-05-27 11:30:21 +0900

    auto-refresh configuration properties using spring cloud bus

---
+++ services/user-service/src/main/java/com/safecornerscoffee/msa/user/security/SecurityProperties.java
@@ -0,0 +1,29 @@
+package com.safecornerscoffee.msa.user.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+@ConfigurationProperties(prefix="token")
+public class SecurityProperties {
+    private String expirationTime;
+    private String secretKey;
+
+    public String getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setExpirationTime(String expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+}
--- services/user-service/src/main/java/com/safecornerscoffee/msa/user/security/WebSecurity.java
+++ services/user-service/src/main/java/com/safecornerscoffee/msa/user/security/WebSecurity.java
@@ -32,6 +32,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/users").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .permitAll()
